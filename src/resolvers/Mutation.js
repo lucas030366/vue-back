4aad090a337cf6ca6c014fb66a5d616b11391290
lucas030366 <lucas_fernandomendes@hotmail.com>
2020-04-22 23:59:49 -0300

    merge

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,107 @@
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const moment = require("moment")
+const { getUserId } = require("../utils")
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+function createRecord(parent, args, context, info) {
+
+  const date = moment(args.date)
+  if(!date.isValid()){
+    throw new Error("Invalid Date!")
+  }
+
+  const userId = getUserId(context)
+  return context.db.mutation.createRecord({
+    data: {
+      user: {
+        connect: { id: userId }
+      },
+      account:{
+        connect: { id: args.accountId }
+      },
+      category:{
+        connect: { id: args.categoryId }
+      },
+      amount: args.amount,
+      type: args.type,
+      date: args.date,
+      description: args.description,
+      tags: args.tags,
+      note: args.note      
+    }
+  }, info)
+}
+
+function createAccount(parent, { description }, context, info) {
+  const userId = getUserId(context)
+  return context.db.mutation.createAccount({
+    data: {
+      description,
+      user: {
+        connect: {
+          id: userId
+        }
+      }
+    }
+  }, info)
+}
+
+function createCategory(parent, { description, operation }, context, info) {
+  const userId = getUserId(context)
+  return context.db.mutation.createCategory({
+    data: {
+      description,
+      operation,
+      user: {
+        connect: {
+          id: userId
+        }
+      }
+    }
+  }, info)
+}
+
+async function login(parent, { email, password }, context, info) {
+
+  const user = await context.db.query.user({ where: { email } })
+
+  if (!user) {
+    throw new Error("Invalid credentials")
+  }
+
+  const valid = await bcrypt.compare(password, user.password)
+  if (!valid) {
+    throw new Error("Invalid credentials")
+  }
+
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "2h" })
+
+  return {
+    token,
+    user
+  }
+
+}
+
+async function signup(parent, args, context, info) {
+
+  const password = await bcrypt.hash(args.password, 10)
+  const user = context.db.mutation.createUser({ data: { ...args, password } })
+
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "2h" })
+
+  return {
+    token,
+    user
+  }
+}
+
+module.exports = {
+  login,
+  signup,
+  createAccount,
+  createCategory,
+  createRecord
+}
---
+++ src/resolvers/Query.js
@@ -0,0 +1,114 @@
+const moment = require("moment")
+const { getUserId } = require("../utils")
+
+function totalBalance(parent, { date }, context, info) {
+  const userId = getUserId(context)
+
+  const env = process.env
+  const dateISO = moment(date, "YYYY-MM-DD").endOf("day").toISOString()
+  const pgSchema = `${env.PRISMA_SERVICE}$${env.PRISMA_STAGE}`
+
+  const mutation = `
+    mutation TotalBalance($database: PrismaDatabase, $query:String!) {
+      executeRaw(database: $database, query: $query)
+    }
+  `
+  const variaveis = {
+    database: "default",
+    query: `
+      SELECT SUM("${pgSchema}"."Record"."amount") as totalbalance
+        FROM "${pgSchema}"."Record"
+
+        JOIN "${pgSchema}"."_RecordToUser"
+        ON "${pgSchema}"."_RecordToUser"."A" = "${pgSchema}"."Record"."id"
+
+        WHERE "${pgSchema}"."_RecordToUser"."B" = '${userId}'
+        AND "${pgSchema}"."Record"."date" <= '${dateISO}'
+
+    `
+  }
+
+  return context.prisma.$graphql(mutation, variaveis)
+    .then(response => {
+      const totalBalance = response.executeRaw[0].totalbalance
+      return totalBalance ? totalBalance : 0
+    })
+}
+
+function records(parent, { month, type, accountsIds, categoriesIds }, context, info) {
+  const userId = getUserId(context)
+
+  let AND = [{ user: { id: userId } }]
+  AND = !type ? AND : [...AND, { type }]
+
+  AND = !accountsIds || accountsIds.length === 0 ? AND : [...AND, { OR: accountsIds.map(id => ({ account: { id } })) }]
+
+  AND = !categoriesIds || categoriesIds.length === 0 ? AND : [...AND, { OR: categoriesIds.map(id => ({ category: { id } })) }]
+
+  if(month){
+    const date = moment(month, "MM-YYYY") // 06-2020
+    const startDate = date.startOf("month").toISOString() // 01-06-2020T:00:00:0000Z
+    const endDate = date.endOf("month").toISOString() // 31-06-2020T:23:59:9999Z
+
+    AND = [...AND, { date_gte: startDate }, { date_lte: endDate }]
+
+  }
+
+  return context.db.query.records({
+    where: { AND },
+    orderBy: "date_ASC"
+  }, info)
+
+}
+
+function user(parent, args, context, info) {
+  const userId = getUserId(context)
+  return context.db.query.user({ where: { id: userId } }, info)
+}
+
+function accounts(parent, args, context, info) {
+  const userId = getUserId(context)
+  return context.db.query.accounts({
+    where: {
+      OR: [
+        {
+          user: {
+            id: userId
+          }
+        },
+        {
+          user: null
+        }
+      ]
+    },
+    orderBy: "description_ASC"
+  }, info)
+}
+
+function categories(parent, { operation }, context, info) {
+  const userId = getUserId(context)
+
+  let AND = [
+    {
+      OR: [
+        { user: { id: userId } },
+        { user: null }
+      ]
+    }
+  ]
+
+  AND = !operation ? AND : [...AND, { operation }]
+
+  return context.db.query.categories({
+    where: { AND },
+    orderBy: "description_ASC"
+  }, info)
+}
+
+module.exports = {
+  user,
+  accounts,
+  categories,
+  records,
+  totalBalance
+}
